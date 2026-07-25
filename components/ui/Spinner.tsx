4f4bf40b58b7b6abb62d356776c8de45e039d31a
Brady <githinjijohn0294@gmail.com>
2026-07-26 00:03:29 +0300

    Changes to UI Spinner

--- components/ui/Spinner.tsx
+++ components/ui/Spinner.tsx
@@ -4,15 +4,17 @@ import { LoaderCircle } from "lucide-react";
 
 interface SpinnerProps {
   size?: number;
+  className?: string;
 }
 
 export default function Spinner({
   size = 20,
+  className = "",
 }: SpinnerProps) {
   return (
     <LoaderCircle
       size={size}
-      className="animate-spin"
+      className={`animate-spin ${className}`}
     />
   );
 }
